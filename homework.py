fa16204abc4e8f1d053dd7a3eabb525f2022c7df
Andrew7567 <92151304+Andrew7567@users.noreply.github.com>
2021-10-29 17:47:48 +0300

    Update homework.py

--- homework.py
+++ homework.py
@@ -120,8 +120,7 @@ def read_package(workout_type: str, data: list) -> Training:
 
 def main(training: Training):
     """Главная функция."""
-    info = training.show_training_info()
-    print(info.get_message())
+    print(InfoMessage.get_message(training.show_training_info()))
 
 
 if __name__ == '__main__':
